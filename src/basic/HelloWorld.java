bc704644036ff1dfb64410828e1f7b67c6cf2bee
tsuji-moto <tsujimoto-s@outlook.com>
2023-06-07 22:59:08 +0900

    [add] 06/07 22:58

---
+++ src/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * HelloWorldクラス
+ *
+ * @author tsuji-moto
+ */
+public class HelloWorld {
+    /**
+     * @param arguments 使用しません。
+     */
+    public static void main(String[] arguments) {
+        System.out.println("HelloWorld!");
+    }
+}
--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,32 @@
 #!/bin/bash -x
+#
+# ---className---
+# compile.sh
+#
+# ---classUsage---
+# bash compile.sh $1
+#   $1:javaClassName
+#
+# ---classInfo---
+# 引数に指定されたjavaクラス名を活用し、対象クラスをコンパイルする。
+# コンパイルしたクラスファイルは、classes配下に作成される。
+#
+
+function resultCheck() {
+  if [[ ${1} == 0 ]]; then
+    echo -e "\n-----SUCCESS-----value[${1}]-----line[${LINENO}]\n"
+  else
+    echo -e "\n-----ERROR-----value[${1}]-----line[${LINENO}]\n"
+  fi
+}
+
+# 各変数に初期値を設定する
+dir_classes="classes"
+java_class_name=$1
+java_class_file="${1}.java"
+
+javac -d ../${dir_classes} -encoding UTF-8 ./${java_class_file}
+# javac -verbose -d ../${dir_classes} -encoding UTF-8 ./${java_class_file}
+resultCheck $?
 
 exit 0
